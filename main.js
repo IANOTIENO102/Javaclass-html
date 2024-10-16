90efa715df294084aa9fc4373f9d3bb91479c721
IAN OTIENO <kagotian@gmail.com>
2024-10-16 16:00:57 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+//get the button element
+const javaclass=document.getElementById(elementid:'colorButton')
+
+//add even listener for the button click
+javaclass.addEventlistener('click',(listener:))
